902f86911661b5c96409cc715e0bef04c48490cf
asrsp <asrsp@inbox.ru>
2022-12-22 12:11:47 +0300

    date 22_12_2022

---
+++ src/main/java/App.java
@@ -0,0 +1,27 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.getMessage());
+        System.out.println(bean1.getMessage());
+
+        Cat cat=
+                (Cat) applicationContext.getBean("Cat");
+        Cat cat1=
+                (Cat) applicationContext.getBean("Cat");
+        System.out.println(cat.getName());
+        System.out.println(cat1.getName());
+
+        System.out.println("Сравниваем первую пару - " + (bean == bean1));
+        System.out.println("Сравниваем вторую пару - " + (cat == cat1));
+
+
+    }
+}
